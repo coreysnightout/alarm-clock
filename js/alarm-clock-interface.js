650a621334e97beacb6b649ae8cd95d44cc861b3
Andrew Dalton, corey redding and justin gomez <andrew+corey+justin@>
2017-06-27 16:22:22 -0700

    add snooze with bugs

--- js/alarm-clock-interface.js
+++ js/alarm-clock-interface.js
@@ -7,23 +7,34 @@ $(document).ready(function(){
     $('#time').text(moment().format('LTS'));
   }, 1000);
 
-
-
-  // alert("test");
-
   var alarmClock = new Clock();
   var array = [];
+
   $("form").submit(function(event) {
     event.preventDefault();
     var time = $("#alarmTime").val();
+    console.log(time);
     array.push(time);
-    // alert(array);
+  });
+
+  $("#snooze").submit(function(event) {
+    event.preventDefault();
+
+    var addFive = parseInt($("#alarmTime").val().slice(3, 5)) + 5;
+    var backToTime = addFive.toString().padStart(2, "0");
+
+    alert(backToTime);
+
+
+
+
+
   });
 
   setInterval(function() {
     var military = [moment().format('HH:mm')];
-    console.log(array);
-    console.log(military);
+    // console.log(array);
+    // console.log(military);
     alarmClock.alarm(military[0], array[0]);
   }, 1000);
 });
--- js/alarm-clock.js
+++ js/alarm-clock.js
@@ -1,10 +1,9 @@
 function Clock(name) {
   this.name = name;
 }
-Clock.prototype.alarm = function(pageClock, alarmTime) {
-
+Clock.prototype.alarm = function(pageClock, alarmTime){
   if(pageClock === alarmTime) {
-    alert("Beeeeeeep boooooop beeeeeeep");
+    console.log("Beeeeeeep boooooop beeeeeeep");
   }
-}
+};
 exports.clockModule = Clock;
